08c107c483e26c493f6e2468e0141eb326238fa8
Jean-Marc Comby <jean-marc.comby@finalspark.com>
2025-03-10 15:21:06 +0100

    try without toml

--- setup.py
+++ setup.py
@@ -1,29 +1,40 @@
 from setuptools import setup, find_packages
-import toml
-
-# Read pyproject.toml
-with open("pyproject.toml", "r") as f:
-    pyproject = toml.load(f)
-
-project = pyproject["project"]
 
 setup(
-    name=project["name"],
-    version=project["version"],
-    author=project["authors"][0]["name"],
-    author_email=project["authors"][0]["email"],
-    description=project["description"],
-    long_description=open(project["readme"]).read(),
+    name="np_utils",
+    version="0.1.0",
+    author="Cyril Achard",
+    author_email="cyril.achard@finalspark.com",
+    description="A collection of utilities for the FinalSpark Neuroplatform",
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    license=project["license"]["file"],
-    classifiers=project["classifiers"],
-    python_requires=project["requires-python"],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=project["dependencies"],
-    package_data={
-        "np_utils": ["src/np_utils/MEA_schema.png"],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "numba",
+    ],
+    extras_require={
+        "all": ["matplotlib", "seaborn", "scipy", "scikit-learn", "numba", "tqdm", "joblib", "h5py", "Pillow", "plotly", "nbformat"],
+        "SPL": ["plotly", "Pillow", "tqdm", "nbformat"],
+        "SSG": ["scikit-learn", "matplotlib", "seaborn", "tqdm", "joblib"],
+        "RRL": ["h5py"],
+        "CCM": ["matplotlib", "seaborn", "numba", "tqdm"],
+        "SSN": ["matplotlib", "tqdm", "seaborn", "np_utils[SPL] @ git+https://github.com/FinalSpark-np/np-utils.git"],
     },
-    extras_require=project["optional-dependencies"],
-    project_urls=project["urls"],
+    package_data={"np_utils": ["src/np_utils/MEA_schema.png"]},
+    project_urls={
+        "Homepage": "https://github.com/FinalSpark-np/np-utils",
+        "Documentation": "https://finalspark-np.github.io/np-docs/welcome.html#navigation",
+        "Source": "https://github.com/FinalSpark-np/np-utils",
+        "Tracker": "https://github.com/FinalSpark-np/np-utils/issues",
+    },
 )
